29c3f70231a968901aa345cfdbfad9ee252bb618
rafaavc <rafavc2508@gmail.com>
2020-12-07 10:36:05 +0000

    Small fix to pages/profile.php

--- src/pages/profile.php
+++ src/pages/profile.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>To The Rescue!</title>
-    <link rel="stylesheet" href="../myProjects/webProject/icofont/css/icofont.min.css">
-    <link rel="stylesheet" type="text/css" href="../style.css" />
-	<link rel="stylesheet" type="text/css" href="profile.css" />
-    <script src="../scripts.js" defer></script>
-</head>
-
-<body>
 <?php
 include_once(dirname(__FILE__) ."/../templates/common/header.php");
 include_once(dirname(__FILE__) ."/../templates/profile/profile_page_header.php");
@@ -34,6 +23,3 @@ include_once(dirname(__FILE__) ."/../templates/profile/profile_page_header.php")
 <?php
 include_once(dirname(__FILE__) ."/../templates/common/footer.php");
 ?>
-
-</body>
-</html>
