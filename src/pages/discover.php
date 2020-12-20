8c14cfee1f906f50e203d015a16fdfbd1b3d3e5c
rafaavc <rafavc2508@gmail.com>
2020-12-20 23:12:16 +0000

    Small bug fix and updated style.min.css

--- src/pages/discover.php
+++ src/pages/discover.php
@@ -4,7 +4,6 @@ require_once(dirname(__FILE__)."/../control/api/pet.php");
 require_once(dirname(__FILE__)."/../control/api/user.php");
 
 require_once(dirname(__FILE__) ."/../templates/common/header.php");
-require_once(dirname(__FILE__) ."/../templates/tiles.php");
 ?>
 
 <?php require_once(dirname(__FILE__)."/../templates/search_form.php"); ?>
